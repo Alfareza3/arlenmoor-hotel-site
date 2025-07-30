bda8e0a9a1915eea4ba044e5fbe1875de516abd8
Dimas_Fahri_Alfareza <dimasfaal@gmail.com>
2025-07-30 10:50:02 +0800

    Arlenmoor

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm sticky-top">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="index.php">The Arlenmoor</a>
+    <a class="navbar-brand fw-bold" href="index.php">🗝️ The Arlenmoor</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
